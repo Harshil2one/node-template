a84cf418c3f20a0489318ce0ce492c30a70c1694
Harshil.21 <85223027+HarshilBabariya@users.noreply.github.com>
2025-07-09 16:32:08 +0530

    Update assert.route.ts

--- routes/assert.route.ts
+++ routes/assert.route.ts
@@ -4,6 +4,38 @@ const assertRouter = express.Router();
 
 const { compare } = assertController;
 
+/**
+ * @openapi
+ * /v1/api/compare-assert:
+ *   get:
+ *     summary: Compare asserts
+ *     description: Just for use cases understanding.
+ *     operationId: compareAsserts
+ *     tags:
+ *       - Assert
+ *     responses:
+ *       200:
+ *         description: Successfully compare asserts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 status:
+ *                   type: number
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: Assert compared successfully..!
+ *       401:
+ *         description: Unauthorized - User can not compare asserts
+ *     servers:
+ *       - url: http://localhost:8000
+ *         description: Local development server
+ */
 assertRouter.route("/compare-assert").get(compare);
 
 export default assertRouter;
