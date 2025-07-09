f3048850451b00d777a28c6bd0b0d4f16c5eb364
Harshil.21 <85223027+HarshilBabariya@users.noreply.github.com>
2025-07-09 16:32:58 +0530

    Update index.ts

--- routes/index.ts
+++ routes/index.ts
@@ -5,19 +5,23 @@ import fileRouter from "./file.route";
 import osRouter from "./os.route";
 import bufferRouter from "./buffer.route";
 import assertRouter from "./assert.route";
-import { APIResponse } from "../helpers/apiResponse";
 import { HTTP_STATUS } from "../enums/status.enum";
+
+const swaggerUI = require("swagger-ui-express");
+const swaggerConfig = require("../config/swagger.config");
 
 const BASE_PATH = "/v1/api";
 
 const appRouter = express.Router();
+
+appRouter.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerConfig));
+
 appRouter.route("/").get((req, res) => {
-  APIResponse(
-    res,
-    true,
-    HTTP_STATUS.SUCCESS,
-    "Welcome to Nodejs template setup!"
-  );
+  res
+    .status(HTTP_STATUS.SUCCESS)
+    .send(
+      "<h1 style='text-align: center; margin-top: 50vh'>Welcome to Nodejs template project setup</h1>"
+    );
 });
 
 export default (app: Application) => {
