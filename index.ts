9629ef0fefe4d183a423bd52f9df024d80f997f2
HarshilBabariya <harshilbabariya01@gmail.com>
2025-11-19 10:46:04 +0530

    update env variables

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import runStandardMiddleware from "./middlewares/standard.middleware";
 import { initializeSocket } from "./config/socket.config";
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 runStandardMiddleware(app);
 
@@ -17,9 +17,7 @@ const startServer = async () => {
 
   const { server } = initializeSocket(app);
   server.listen(port, "0.0.0.0", () => {
-    console.log(
-      `Server is running on port: ${port}/${process.env.BASE_URL}`
-    );
+    console.log(`Server is running on port: ${port}`);
   });
 };
 
