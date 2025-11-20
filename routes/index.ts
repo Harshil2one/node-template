70e643c7c2fa394961caf7d4f0e8ad5c3bf55d0a
HarshilBabariya <harshilbabariya01@gmail.com>
2025-11-20 12:06:03 +0530

    Revert "update url"
    This reverts commit 3f3715d1d7faf82f930a3fc3f9f8d851e9894999.

--- routes/index.ts
+++ routes/index.ts
@@ -14,17 +14,11 @@ import footerRouter from "./footer.route";
 import csvRouter from "./csv.route";
 import notificationRouter from "./notifications.route";
 import tokenRouter from "./token.route";
-import dotenv from "dotenv";
 
 import admin from "firebase-admin";
-dotenv.config();
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    projectId: process.env.FIREBASE_PROJECT_ID,
-  }),
+  credential: admin.credential.cert(require("../serviceAccountKey.json")),
 });
 
 const swaggerUI = require("swagger-ui-express");
